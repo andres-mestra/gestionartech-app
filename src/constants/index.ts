02897529784d35d3353558793fd04a2060b36552
andres-mestra <andresmestra11a@gmail.com>
2024-07-30 00:16:37 -0500

    build: set production url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:1339/api'
-export const PUBLIC_URL = 'http://localhost:1339'
+export const API_URL = 'https://test-6ikx.onrender.com/api'
+export const PUBLIC_URL = 'https://test-6ikx.onrender.com'
